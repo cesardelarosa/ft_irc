0ff82c9a6810c3461f648c94cfafd5767446a2c8
César de la Rosa <code@cesardelarosa.xyz>
2025-10-05 18:07:35 +0200

    Command Handler .hpp extended

--- include/CommandHandler.hpp
+++ include/CommandHandler.hpp
@@ -3,7 +3,10 @@
 #ifndef COMMANDHANDLER_HPP
 #define COMMANDHANDLER_HPP
 
+#include "Client.hpp"
+#include <map>
 #include <string>
+#include <vector>
 
 class Server;
 class Client;
@@ -17,6 +20,17 @@ class CommandHandler {
 
   private:
 	Server *_server;
+
+	typedef void (CommandHandler::*CommandFunction)(
+		Client &client, const std::vector<std::string> &args);
+
+	std::map<std::string, CommandFunction> _commands;
+
+	void	_parseAndExecute(Client &client, const std::string &raw_command);
+
+	void _handleNick(Client &client, const std::vector<std::string> &args);
+	void _handleUser(Client &client, const std::vector<std::string> &args);
+	void _handlePass(Client &client, const std::vector<std::string> &args);
 
 	CommandHandler();
 	CommandHandler(CommandHandler const &src);
